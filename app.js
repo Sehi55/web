afb8cccf4bd017519210a720f5c59a98616a03ef
Sehi55 <sehi1028@naver.com>
2025-05-27 13:34:25 +0900

    fix:app.js

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
-var express = require('express'), http= require('http');
+var express = require('express'), http= require('http'), path=require('path');
 var static = require('serve-static');
 var app = express();
+var router = express.Router();
 
 app.set('port',process.env.PORT||8080);
 app.set('host','127.0.0.1');
 
+app.get('/',(req,res)=>{
+	res.redirect('/source/jquery.html');
+});
 app.use(static(__dirname));
+
+app.use(express.urlencoded());
+app.use(express.json());
+
 
 http.createServer(app).listen(app.get('port'), app.get('host'),()=>{
 	console.log("Express server running at "+ app.get('port')+ app.get('host'));
